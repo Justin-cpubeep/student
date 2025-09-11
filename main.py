b0452ae4b63e26302bf8a981f774f76459beb7cc
ljh <qw803qw803@gmail.com>
2025-09-11 10:46:42 +0900

    기능 완성

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ stdlist=[
     {'id': '202506', 'name': 'jeong', 'korean': 21, 'english': 34, 'math': 11},
     {'id': '202507', 'name': 'choi', 'korean': 43, 'english': 56, 'math': 39}
 ]
-accumulatedID=3
+accumulatedID=8
 page=-1
-order=input(
+
+while True:
+    order=input(
     """
     C: 학생 추가,
     D: 학생 삭제,
@@ -18,10 +20,20 @@ order=input(
     M: 성적 수정,
     Q: 작업 종료
     """
-).upper()
-while True:
+    ).upper()
     if order=="C":
         print("학생을 생성합니다.")
+        student={"korean":None,"math":None,"english":None}
+        # 100명 이내라 가정
+        s_id="2025"+"0"*(2-len(str(accumulatedID)))+str(accumulatedID)
+        s_name=input("학생 이름을 입력해주세요: ")
+        student["id"]=s_id
+        student["name"]=s_name
+        accumulatedID+=1
+        if page==-1:
+            page=0
+        stdlist.append(student)
+        print(stdlist)
     elif order=="D":
         print("학생을 삭제합니다.")
     elif order=="V":
