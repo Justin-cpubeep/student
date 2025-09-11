7776d1eaf7f8c0765a8e8a18dfff13d81a0d006a
ljh <qw803qw803@gmail.com>
2025-09-11 10:06:01 +0900

    main 생성

---
+++ main.py
@@ -0,0 +1,24 @@
+order=input(
+    """
+    C: 학생 추가,
+    D: 학생 삭제,
+    V: 학생 조회,
+    I: 성적 입력,
+    M: 성적 수정,
+    Q: 작업 종료
+    """
+).upper()
+while True:
+    if order=="C":
+        print("학생을 생성합니다.")
+    elif order=="D":
+        print("학생을 삭제합니다.")
+    elif order=="V":
+        print("학생을 조회합니다.")
+    elif order=="I":
+        print("학생 성적을 입력합니다.")
+    elif order=="M":
+        print("학생 성적을 수정합니다.")
+    elif order=="Q":
+        print("작업을 종료합니다.")
+        break
